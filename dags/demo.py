a7533528a7e6f76f03276945060ca9f51d0ff5d6
Andreas Rjasanow <andrjas@gmail.com>
2023-12-07 18:19:59 +0100

    v2

--- dags/demo.py
+++ dags/demo.py
@@ -4,6 +4,19 @@ from pathlib import Path
 
 from airflow.decorators import dag, task
 
+@task
+def file_orders():
+    """
+    get raw_orders.csv and save to data/raw_orders
+    """
+    url = "https://raw.githubusercontent.com/dbt-labs/jaffle_shop/main/seeds/raw_orders.csv"
+    data = requests.get(url)
+    folder = Path("data/raw_orders")
+    folder.mkdir(parents=True, exist_ok=True)
+    path = folder / f"raw_orders.csv"
+    path.write_text(data.text)
+
+
 @dag(
     schedule="@daily",
     start_date=pendulum.datetime(2023, 12, 1, tz="UTC"),
@@ -14,6 +27,7 @@ def demo_dag():
     """
     a simple demo dag
     """
-    pass
+    file_orders()
+
 
 demo_dag()
