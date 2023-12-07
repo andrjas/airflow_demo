01e99da65451f94a30068ab3ac0a3a868c3108fb
Andreas Rjasanow <andrjas@gmail.com>
2023-12-07 18:19:25 +0100

    v1

--- dags/demo.py
+++ dags/demo.py
@@ -3,3 +3,17 @@ import requests
 from pathlib import Path
 
 from airflow.decorators import dag, task
+
+@dag(
+    schedule="@daily",
+    start_date=pendulum.datetime(2023, 12, 1, tz="UTC"),
+    catchup=False,
+    tags=["example"],
+)
+def demo_dag():
+    """
+    a simple demo dag
+    """
+    pass
+
+demo_dag()
